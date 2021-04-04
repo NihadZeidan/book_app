9967f3fb79637e8af25aec0c09fc022e6ffcb3f2
NihadZeidan <nihadzeidan@gmail.com>
2021-04-04 18:04:59 +0300

    feature 4 and 6

--- server.js
+++ server.js
@@ -13,28 +13,32 @@ const PORT = process.env.PORT || 4444
 // MiddleWare to direct your express
 app.use(express.urlencoded());
 
-// // create static path
-// app.get('/static')
 
 // creat path for form
-app.get('/searches/new',formHandler)
-app.post('/searches/show',resultHandler)
-// To set the view engine to server-side template   
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.get('/searches/new', formHandler)
+app.post('/searches', resultHandler)
 
+
+// To set the view engine to server-side template   
+app.use(express.static('public/styles'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
     response.render('pages/index');
 });
 
-function formHandler(req,res) {
-  res.render('pages/searches/new')
-}  
+app.use("*", errorHandler)
 
-function resultHandler(req,res) {
+// ---------------------------------------
+
+function formHandler(req, res) {
+    res.render('pages/searches/new')
+}
+
+function resultHandler(req, res) {
     let url = `https://www.googleapis.com/books/v1/volumes`
-    const {searchBy,search} = req.body;
+    const { searchBy, search } = req.body;
     let searchQuery = {};
     if (searchBy === 'title') {
         searchQuery['q'] = `+intitle:${search}`
@@ -42,25 +46,34 @@ function resultHandler(req,res) {
         searchQuery['q'] = `+inauthor:${search}`
     }
 
- superAgent.get(url).query(searchQuery).then( (result)=> {return result.body.items.map(
-   book => {
-     let newBook = new Book(book);
-     if (newBook.image_url === '') {
-     newBook.image_url=  `https://i.imgur.com/J5LVHEL.jpg`
-    };
-    return newBook;
-   }
- )}).then ( resultNew => {
-     res.render('searches/show', {UserBooks: resultNew.slice(0,11)})
- }
- )
+    superAgent.get(url).query(searchQuery).then((result) => {
+        return result.body.items.map(
+            book => {
+                let newBook = new Book(book);
+                if (newBook.image_url === '') {
+                    newBook.image_url = 'https://i.imgur.com/J5LVHEL.jpg'
+                };
+                return newBook;
+            }
+        )
+    }).then(resultNew => {
+        res.render('pages/show', { UserBooks: resultNew.slice(0, 11) })
+    }).catch(res => {
+        res.render("pages/error");
+    })
 }
 
+function errorHandler(req, res) {
+    res.render("pages/error");
+}
+
+
+// -----------------------------------------
 function Book(dataBook) {
-this.authors = dataBook.volumeInfo.authors;
-this.title= dataBook.volumeInfo.title;
-this.description = dataBook.volumeInfo.description;
-this.image_url = dataBook.volumeInfo.imageLinks;
+    this.authors = dataBook.volumeInfo.authors;
+    this.title = dataBook.volumeInfo.title;
+    this.description = dataBook.volumeInfo.description;
+    this.image_url = dataBook.volumeInfo.imageLinks.smallThumbnail;
 }
 
 
